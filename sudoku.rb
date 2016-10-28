f822715beaeacd4a5bcb63f77b14bd1a13eebe10
Pablo R. Hernandez & Sophia <pair+prh90.sophiav@devbootcamp.com>
2016-10-28 15:36:23 -0700

    Merged methods

--- sudoku.rb
+++ sudoku.rb
@@ -20,3 +20,33 @@ end
 # form `solve` returns.
 def pretty_board(board)
 end
+
+
+def create_board(string)
+  board_rows = string.chars.each_slice(9).to_a
+end
+
+ p create_board("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
+
+ def search_dash(array)
+   array.each_with_index do |nested_array, array_index|
+      nested_array.each_with_index do |space, nested_index|
+        if space == "-"
+          num = possibility(nested_index)
+          nested_array[nested_index] = num
+        end
+      end
+    end
+  end
+def possibility(column_row,box)
+end
+
+
+def column_row (array1,nested_index,array_index)
+  column_array=[]
+  array1.each do |nested_array|
+    column_array << nested_array[nested_index]
+  end
+  row_array = array1[array_index]
+end
+
