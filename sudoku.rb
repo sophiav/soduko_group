9dfcaf8bf8db6d0cb73ce01cd1775f601384add0
Pablo R. Hernandez & Sophia <pair+prh90.sophiav@devbootcamp.com>
2016-10-28 18:05:09 -0700

    update

--- sudoku.rb
+++ sudoku.rb
@@ -23,30 +23,75 @@ end
 
 
 def create_board(string)
+  string.gsub!(/-/,"0")
   board_rows = string.chars.each_slice(9).to_a
-end
-
- p create_board("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
-
- def search_dash(array)
-   array.each_with_index do |nested_array, array_index|
-      nested_array.each_with_index do |space, nested_index|
-        if space == "-"
-          num = possibility(nested_index)
-          nested_array[nested_index] = num
-        end
-      end
+  board_rows.map! do |array|
+    array.map! do |num|
+      num = num.to_i
     end
   end
-def possibility(column_row,box)
+  array.each_with_index do |nested_array, array_index|
+      nested_array.each_with_index do |space, nested_index|
+        if space == 0
+           # num = possibility(nested_index)
+           answer = nested_array
+           column = column_array(board_rows,array_index,nested_index)
+        end
+        answer
+        column
+      end
+    end
+  # answer = search_dash(board_rows)
+  num = ((1..9).to_a) -answer
 end
 
 
-def column_row (array1,nested_index,array_index)
+
+def column_array (array1,nested_index,array_index)
   column_array=[]
   array1.each do |nested_array|
     column_array << nested_array[nested_index]
   end
-  row_array = array1[array_index]
 end
+ # def search_dash(array)
+ #  answer=[]
+ #   array.each_with_index do |nested_array, array_index|
+ #      nested_array.each_with_index do |space, nested_index|
+ #        if space == 0
+ #           # num = possibility(nested_index)
+ #           answer = nested_array
+ #        end
+ #      end
+ #    end
+ #    answer
+ #  end
+
+# def possibility(column_row,box)
+# end
+def box()
+  hash = {
+    1 => [board[0][0..2], board[1][0..2], board[2][0..2]],
+    2 => [board[0][3..5], board[1][3..5], board[2][3..5]],
+    3 => [board[0][6..9], board[1][6..9], board[2][6..9]],
+
+    4 => [board[3][0..2], board[4][0..2], board[5][0..2]],
+    5 => [board[3][3..5], board[4][3..5], board[5][3..5]],
+    6 => [board[3][6..9], board[4][6..9], board[5][6..9]],
+
+    7 => [board[6][0..2], board[7][0..2], board[8][0..2]],
+    8 => [board[6][3..5], board[7][3..5], board[8][3..5]],
+    9 => [board[6][6..9], board[7][6..9], board[8][6..9]]
+  }
+end
+
+create_board("39128657448735912665271483987543169221396748596452871314967325853814296772689534-")
+
+
+
+
+
+
+
+
+
 
